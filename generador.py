247b6a73d78731aaa9b5610768a7acad9b51cca2
Antonella Pellegrini <antopell29@gmail.com>
2025-03-16 14:57:39 -0300

    Cambio tamaño de tabs, cambio tabs por espacios en lo que escribe y cambio el CLI_ID por cada cliente

--- generador.py
+++ generador.py
@@ -2,48 +2,49 @@ import sys
 
 def generar(nombre, cant_clientes):
     
-    with open(nombre, "w") as archivo:
-        archivo.write("name: tp0\n")
-        archivo.write("services:\n")
+	with open(nombre, "w") as archivo:
+		archivo.write("name: tp0\n")
+		archivo.write("services:\n")
 
-        # server
-        archivo.write("\tserver:\n")
-        archivo.write("\t\tcontainer_name: server\n")
-        archivo.write("\t\timage: server:latest\n")
-        archivo.write("\t\tentrypoint: python3 /main.py\n")
-        archivo.write("\t\tenvironment:\n")
-        archivo.write("\t\t\t- PYTHONUNBUFFERED=1\n")
-        archivo.write("\t\t\t- LOGGING_LEVEL=DEBUG\n")
-        archivo.write("\t\tnetworks:\n")
-        archivo.write("\t\t\t- testing_net\n")
+		# server
+		archivo.write("  server:\n")
+		archivo.write("    container_name: server\n")
+		archivo.write("    image: server:latest\n")
+		archivo.write("    entrypoint: python3 /main.py\n")
+		archivo.write("    environment:\n")
+		archivo.write("      - PYTHONUNBUFFERED=1\n")
+		archivo.write("      - LOGGING_LEVEL=DEBUG\n")
+		archivo.write("    networks:\n")
+		archivo.write("      - testing_net\n")
 
-        archivo.write("\n")
+		archivo.write("\n")
 
-        # clientes
-        for i in range(cant_clientes):
-            nombre_cliente = "client" + str(i + 1)
-            archivo.write("\t" + nombre_cliente + ":\n")
-            archivo.write("\t\tcontainer_name: " + nombre_cliente + "\n")
-            archivo.write("\t\timage: client:latest\n")
-            archivo.write("\t\tentrypoint: /client\n")
-            archivo.write("\t\tenvironment:\n")
-            archivo.write("\t\t\t- CLI_ID=1\n")
-            archivo.write("\t\t\t- CLI_LOG_LEVEL=DEBUG\n")
-            archivo.write("\t\tnetworks:\n")
-            archivo.write("\t\t\t- testing_net\n")
-            archivo.write("\t\tdepends_on:\n")
-            archivo.write("\t\t\t- server\n")
-            archivo.write("\n")
-            
-        # network
-        archivo.write("networks:\n")
-        archivo.write("\ttesting_net:\n")
-        archivo.write("\t\tipam:\n")
-        archivo.write("\t\t\tdriver: default\n")
-        archivo.write("\t\t\tconfig:\n")
-        archivo.write("\t\t\t\t- subnet: 172.25.125.0/24\n")
+		# clientes
+		for i in range(1, cant_clientes+1):
+			num_string = str(i)
+			nombre_cliente = "client" + num_string
+			archivo.write("  " + nombre_cliente + ":\n")
+			archivo.write("    container_name: " + nombre_cliente + "\n")
+			archivo.write("    image: client:latest\n")
+			archivo.write("    entrypoint: /client\n")
+			archivo.write("    environment:\n")
+			archivo.write("      - CLI_ID=" + num_string + "\n")
+			archivo.write("      - CLI_LOG_LEVEL=DEBUG\n")
+			archivo.write("    networks:\n")
+			archivo.write("      - testing_net\n")
+			archivo.write("    depends_on:\n")
+			archivo.write("      - server\n")
+			archivo.write("\n")
+				
+		# network
+		archivo.write("networks:\n")
+		archivo.write("  testing_net:\n")
+		archivo.write("    ipam:\n")
+		archivo.write("      driver: default\n")
+		archivo.write("      config:\n")
+		archivo.write("        - subnet: 172.25.125.0/24\n")
 
-    return
+	return
 
 
 

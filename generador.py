c180b15ec414739d814f8ddaeeaa1c35e6f7115c
Antonella Pellegrini <antopell29@gmail.com>
2025-03-16 15:02:46 -0300

    Cambio muchas escrituras por una sola de un string mas largo

--- generador.py
+++ generador.py
@@ -3,46 +3,51 @@ import sys
 def generar(nombre, cant_clientes):
     
 	with open(nombre, "w") as archivo:
-		archivo.write("name: tp0\n")
-		archivo.write("services:\n")
+		contenido = "name: tp0\n"
+		contenido += "services:\n"
+		contenido += "name: tp0\n"
+		contenido += "services:\n"
 
 		# server
-		archivo.write("  server:\n")
-		archivo.write("    container_name: server\n")
-		archivo.write("    image: server:latest\n")
-		archivo.write("    entrypoint: python3 /main.py\n")
-		archivo.write("    environment:\n")
-		archivo.write("      - PYTHONUNBUFFERED=1\n")
-		archivo.write("      - LOGGING_LEVEL=DEBUG\n")
-		archivo.write("    networks:\n")
-		archivo.write("      - testing_net\n")
+		contenido += "  server:\n"
+		contenido += "    container_name: server\n"
+		contenido += "    image: server:latest\n"
+		contenido += "    entrypoint: python3 /main.py\n"
+		contenido += "    environment:\n"
+		contenido += "      - PYTHONUNBUFFERED=1\n"
+		contenido += "      - LOGGING_LEVEL=DEBUG\n"
+		contenido += "    networks:\n"
+		contenido += "      - testing_net\n"
 
-		archivo.write("\n")
+		contenido += "\n"
 
 		# clientes
 		for i in range(1, cant_clientes+1):
 			num_string = str(i)
 			nombre_cliente = "client" + num_string
-			archivo.write("  " + nombre_cliente + ":\n")
-			archivo.write("    container_name: " + nombre_cliente + "\n")
-			archivo.write("    image: client:latest\n")
-			archivo.write("    entrypoint: /client\n")
-			archivo.write("    environment:\n")
-			archivo.write("      - CLI_ID=" + num_string + "\n")
-			archivo.write("      - CLI_LOG_LEVEL=DEBUG\n")
-			archivo.write("    networks:\n")
-			archivo.write("      - testing_net\n")
-			archivo.write("    depends_on:\n")
-			archivo.write("      - server\n")
-			archivo.write("\n")
+			contenido += "  " + nombre_cliente + ":\n"
+			contenido += "    container_name: " + nombre_cliente + "\n"
+			contenido += "    image: client:latest\n"
+			contenido += "    entrypoint: /client\n"
+			contenido += "    environment:\n"
+			contenido += "      - CLI_ID=" + num_string + "\n"
+			contenido += "      - CLI_LOG_LEVEL=DEBUG\n"
+			contenido += "    networks:\n"
+			contenido += "      - testing_net\n"
+			contenido += "    depends_on:\n"
+			contenido += "      - server\n"
+			contenido += "\n"
 				
 		# network
-		archivo.write("networks:\n")
-		archivo.write("  testing_net:\n")
-		archivo.write("    ipam:\n")
-		archivo.write("      driver: default\n")
-		archivo.write("      config:\n")
-		archivo.write("        - subnet: 172.25.125.0/24\n")
+		contenido += "networks:\n"
+		contenido += "  testing_net:\n"
+		contenido += "    ipam:\n"
+		contenido += "      driver: default\n"
+		contenido += "      config:\n"
+		contenido += "        - subnet: 172.25.125.0/24\n"
+
+		
+		archivo.write(contenido)
 
 	return
 

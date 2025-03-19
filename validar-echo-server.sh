a551d596cbbd451b216d8531c615812420d9284c
Antonella Pellegrini <antopell29@gmail.com>
2025-03-19 14:15:38 -0300

    Saco todo lo extra a correr el comando

--- validar-echo-server.sh
+++ validar-echo-server.sh
@@ -1,21 +1,15 @@
 #!/bin/bash
 
-# docker setup
-sh ./generar-compose.sh docker-compose-dev.yaml 1
-make docker-compose-up > /dev/null 2>&1
-
 PORT=$(grep "SERVER_PORT = " ./server/config.ini  | cut -d ' ' -f3)
 IP=$(grep "SERVER_IP = " ./server/config.ini  | cut -d ' ' -f3)
 
 TESTING_STR='testing'
 
-RES=$(docker exec client1 sh -c "echo $TESTING_STR | nc $IP $PORT")
+RES=$(docker run --net tp0_testing_net busybox:latest sh -c "echo $TESTING_STR | nc $IP $PORT")
 
-if [[ "$RES" == "$TESTING_STR" ]]
+if [ "$RES" = "$TESTING_STR" ]
 then
   echo "action: test_echo_server | result: success"
 else
   echo "action: test_echo_server | result: fail"
 fi
-
-make docker-compose-down  > /dev/null 2>&1

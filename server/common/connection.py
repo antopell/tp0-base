5a2616bf1261c594d18be283f90892c749dbb925
Antonella Pellegrini <antopell29@gmail.com>
2025-03-27 11:45:17 -0300

    Agrego concurrencia, primer borrador, mucho codigo comentado, feo

---
+++ server/common/connection.py
@@ -0,0 +1,144 @@
+import socket
+import logging
+
+from protocol.protocol import *
+from common.utils import *
+from multiprocessing import Condition, Value, Process, Lock
+
+
+
+class Connection():
+  def __init__(self, client_sock, has_winners, winners_ready, amount_agencies, clients_map, winners_map, lock_bets):
+    self.client_sock = client_sock
+    self.has_winners = has_winners
+    self.protocol = Protocol()
+    self.winners_ready = winners_ready
+    self.amount_agencies = amount_agencies
+    self.clients_map = clients_map
+    self.winners_map = winners_map
+    self.lock_bets = lock_bets
+
+  def run(self):
+    """
+    Read message from a specific client socket and closes the socket
+
+    If a problem arises in the communication with the client, the
+    client socket will also be closed
+    """
+    try:
+      batch_ended = False
+      agency_number = -1
+      while not batch_ended:
+        action, message = self.__get_action()
+        if action == ActionType.CLOSE:
+          break
+        if action == ActionType.WAIT_FOR_WINNERS:
+          agency_number = self.__get_confirmation(message)
+          break
+        
+        # action == ActionType.GET_BETS:
+        bets, amount, batch_ended = self.__get_bets(message)
+        with self.lock_bets:
+          store_bets(bets)
+        if not batch_ended:
+          success = len(bets) == amount
+          if success:
+            logging.info(f'action: apuesta_recibida | result: success | cantidad: {len(bets)}')
+          else:
+            logging.info(f'action: apuesta_recibida | result: fail | cantidad: {len(bets)}')
+      
+          self.__send_ack(success)
+      
+      self.clients_map[agency_number] = self.client_sock
+      if len(self.clients_map) == self.amount_agencies:
+        self.__define_winners()
+      self.__send_winners(agency_number)
+    except OSError as e:
+      logging.error(f"action: receive_message | result: fail | error: {e}")
+    finally:
+      # client_sock.shutdown(socket.SHUT_WR) # allows reading
+      self.client_sock.close()
+
+  def __define_winners(self):
+      logging.info("action: sorteo | result: success")
+      with self.has_winners, self.winners_ready.get_lock():
+          for winning_bet in load_bets():
+              winner_list = self.winners_map.get(winning_bet.agency, [])
+              self.winners_map[winning_bet.agency] = winner_list
+              if not has_won(winning_bet):
+                  continue
+              winner_list.append(winning_bet.document)
+          self.winners_ready.value = True
+          self.has_winners.notify_all()
+  
+  def __define_winners(self):
+    logging.info("action: sorteo | result: success")
+    with self.has_winners, self.winners_ready.get_lock():
+      for winning_bet in load_bets():
+        winner_list = self.winners_map.get(winning_bet.agency, [])
+        self.winners_map[winning_bet.agency] = winner_list
+        if not has_won(winning_bet):
+          continue
+        winner_list.append(winning_bet.document)
+      self.winners_ready.value = True
+      self.has_winners.notify_all()
+  
+  def __get_action(self) -> ActionType: 
+    read_amount = self.protocol.define_action_buffer_size()
+    action = bytearray()
+    closed_socket = self.__full_recv(action, read_amount)
+    
+    if closed_socket:
+        return ActionType.CLOSE, action
+    return self.protocol.decode_action(action), action
+
+  def __get_bets(self, action: bytearray):
+    read_amount = self.protocol.define_initial_buffer_size()
+    message = action
+    closed_socket = self.__full_recv(message, read_amount)
+    if closed_socket:
+        return [], 0, closed_socket
+    read_amount = self.protocol.define_msg_len(message)
+    closed_socket = self.__full_recv(message, read_amount)
+    bets, amount_bets = self.protocol.decode(message)
+    return bets, amount_bets, closed_socket
+  
+  def __full_recv(self, buffer: bytearray, amount_to_read: int) -> bool:
+    amount_read = 0
+    while amount_read < amount_to_read:
+      msg = self.client_sock.recv(amount_to_read - amount_read)
+      if len(msg) == 0:
+        return True
+      buffer.extend(msg)
+      amount_read += len(msg)
+    return False
+
+  def __send_ack(self, result: bool):
+    message = self.protocol.create_ack_msg(result)
+    self.client_sock.sendall(message)
+
+  def __get_confirmation(self, action: bytearray) -> int:
+    read_amount = self.protocol.define_buffer_size_confirmation()
+    message = action
+    closed_socket = self.__full_recv(message, read_amount)
+    if closed_socket:
+      return -1
+    return self.protocol.decode_confirmation(message)
+    
+  def __send_winners(self, agency_number):
+    with self.has_winners:
+      has_to_wait = True
+      with self.winners_ready.get_lock():
+        has_to_wait = not self.winners_ready.value
+
+      if has_to_wait:
+        self.has_winners.wait()
+
+      message = self.protocol.create_winners_msg(self.winners_map[agency_number])
+      try: 
+        self.client_sock.sendall(message)
+      except OSError as e:
+        logging.error(f"action: send_winners | result: fail | error: {e}")
+      finally:
+        # self.client_sock.shutdown(socket.SHUT_WR) # allows reading
+        self.client_sock.close()
--- server/common/server.py
+++ server/common/server.py
@@ -1,9 +1,12 @@
 import socket
 import logging
 import signal
+from multiprocessing import Condition, Value, Process, Manager, Lock
+from ctypes import c_bool
 
 from protocol.protocol import *
 from common.utils import *
+from common.connection import *
 
 class Server:
     def __init__(self, port, listen_backlog, amount_agencies):
@@ -34,60 +37,80 @@ class Server:
         signal.signal(signal.SIGTERM, self._graceful_exit)
         signal.signal(signal.SIGINT, self._graceful_exit)
 
-        clients_map = dict()
-        while self.continue_running and len(clients_map) != self.amount_agencies:
-            client_sock = self.__accept_new_connection()
-            self.__handle_client_connection(clients_map, client_sock)
+        winners_ready = Value(c_bool, False)
+        has_winners = Condition()
+        lock_bets = Lock()
         
-        logging.info("action: sorteo | result: success")
-        clients_winners_map = dict()
-        for winning_bet in load_bets():
-            winner_list = clients_winners_map.get(winning_bet.agency, [])
-            clients_winners_map[winning_bet.agency] = winner_list
-            if not has_won(winning_bet):
-                continue
-            winner_list.append(winning_bet.document) 
-        self.__send_winners(clients_map, clients_winners_map)
+        with Manager() as manager:
+            clients_map = manager.dict()
+            winners_map = manager.dict()
+
+            processes = []
+            while self.continue_running:
+                client_sock = self.__accept_new_connection()
+                client = Connection(client_sock, has_winners, winners_ready, self.amount_agencies, clients_map, winners_map, lock_bets)
+                process = Process(target=client.run)
+                processes.append(process)
+                process.start()
+                logging.info("POST START")
+        
+        
 
 
 
-    def __handle_client_connection(self, clients_map, client_sock):
-        """
-        Read message from a specific client socket and closes the socket
+    # def __handle_client_connection(self, client_sock, has_winners):
+    #     """
+    #     Read message from a specific client socket and closes the socket
 
-        If a problem arises in the communication with the client, the
-        client socket will also be closed
-        """
-        try:
-            batch_ended = False
-            agency_number = -1
-            while not batch_ended:
-                action, message = self.__get_action(client_sock)
-                if action == ActionType.CLOSE:
-                    break
-                if action == ActionType.WAIT_FOR_WINNERS:
-                    agency_number = self.__get_confirmation(client_sock, message)
-                    break
+    #     If a problem arises in the communication with the client, the
+    #     client socket will also be closed
+    #     """
+    #     try:
+    #         batch_ended = False
+    #         agency_number = -1
+    #         while not batch_ended:
+    #             action, message = self.__get_action(client_sock)
+    #             if action == ActionType.CLOSE:
+    #                 break
+    #             if action == ActionType.WAIT_FOR_WINNERS:
+    #                 agency_number = self.__get_confirmation(client_sock, message)
+    #                 break
                 
-                # action == ActionType.GET_BETS:
-                bets, amount, batch_ended = self.__get_bets(client_sock, message)
-                store_bets(bets)
-                if not batch_ended:
-                    success = len(bets) == amount
-                    if success:
-                        logging.info(f'action: apuesta_recibida | result: success | cantidad: {len(bets)}')
-                    else:
-                        logging.info(f'action: apuesta_recibida | result: fail | cantidad: {len(bets)}')
+    #             # action == ActionType.GET_BETS:
+    #             bets, amount, batch_ended = self.__get_bets(client_sock, message)
+    #             store_bets(bets)
+    #             if not batch_ended:
+    #                 success = len(bets) == amount
+    #                 if success:
+    #                     logging.info(f'action: apuesta_recibida | result: success | cantidad: {len(bets)}')
+    #                 else:
+    #                     logging.info(f'action: apuesta_recibida | result: fail | cantidad: {len(bets)}')
                 
-                    self.__send_ack(success, client_sock)
+    #                 self.__send_ack(success, client_sock)
             
-            clients_map[agency_number] = client_sock
-        except OSError as e:
-            logging.error(f"action: receive_message | result: fail | error: {e}")
-        finally:
-            # client_sock.shutdown(socket.SHUT_WR) # allows reading
-            # client_sock.close()
-            pass
+    #         self.clients_map[agency_number] = client_sock
+    #         if len(self.clients_map) == self.amount_agencies:
+    #             self.__define_winners(has_winners)
+    #         self.__send_winners(client_sock, agency_number, has_winners)
+    #     except OSError as e:
+    #         logging.error(f"action: receive_message | result: fail | error: {e}")
+    #     finally:
+    #         # client_sock.shutdown(socket.SHUT_WR) # allows reading
+    #         client_sock.close()
+
+    # def __define_winners(self, has_winners):
+    #     logging.info("action: sorteo | result: success")
+    #     with has_winners, self.winners_ready.get_lock():
+    #         for winning_bet in load_bets():
+    #             winner_list = self.winners_map.get(winning_bet.agency, [])
+    #             self.winners_map[winning_bet.agency] = winner_list
+    #             if not has_won(winning_bet):
+    #                 continue
+    #             winner_list.append(winning_bet.document)
+    #         self.winners_ready.value = True
+    #         has_winners.notify_all()
+
+        
 
     def __accept_new_connection(self):
         """
@@ -103,56 +126,65 @@ class Server:
         logging.info(f'action: accept_connections | result: success | ip: {addr[0]}')
         return c
     
-    def __get_action(self, client_sock: socket.socket) -> ActionType: 
-        read_amount = self.protocol.define_action_buffer_size()
-        action = bytearray()
-        closed_socket = self.__full_recv(action, read_amount, client_sock)
+    # def __get_action(self, client_sock: socket.socket) -> ActionType: 
+    #     read_amount = self.protocol.define_action_buffer_size()
+    #     action = bytearray()
+    #     closed_socket = self.__full_recv(action, read_amount, client_sock)
         
-        if closed_socket:
-            return ActionType.CLOSE, action
-        return self.protocol.decode_action(action), action
+    #     if closed_socket:
+    #         return ActionType.CLOSE, action
+    #     return self.protocol.decode_action(action), action
 
-    def __get_bets(self, client_sock: socket.socket, action: bytearray):
-        read_amount = self.protocol.define_initial_buffer_size()
-        message = action
-        closed_socket = self.__full_recv(message, read_amount, client_sock)
-        if closed_socket:
-            return [], 0, closed_socket
-        read_amount = self.protocol.define_msg_len(message)
-        closed_socket = self.__full_recv(message, read_amount, client_sock)
-        bets, amount_bets = self.protocol.decode(message)
-        return bets, amount_bets, closed_socket
+    # def __get_bets(self, client_sock: socket.socket, action: bytearray):
+    #     read_amount = self.protocol.define_initial_buffer_size()
+    #     message = action
+    #     closed_socket = self.__full_recv(message, read_amount, client_sock)
+    #     if closed_socket:
+    #         return [], 0, closed_socket
+    #     read_amount = self.protocol.define_msg_len(message)
+    #     closed_socket = self.__full_recv(message, read_amount, client_sock)
+    #     bets, amount_bets = self.protocol.decode(message)
+    #     return bets, amount_bets, closed_socket
         
-    def __full_recv(self, buffer: bytearray, amount_to_read: int, socket: socket.socket) -> bool:
-        amount_read = 0
-        while amount_read < amount_to_read:
-            msg = socket.recv(amount_to_read - amount_read)
-            if len(msg) == 0:
-                return True
-            buffer.extend(msg)
-            amount_read += len(msg)
-        return False
+    # def __full_recv(self, buffer: bytearray, amount_to_read: int, socket: socket.socket) -> bool:
+    #     amount_read = 0
+    #     while amount_read < amount_to_read:
+    #         msg = socket.recv(amount_to_read - amount_read)
+    #         if len(msg) == 0:
+    #             return True
+    #         buffer.extend(msg)
+    #         amount_read += len(msg)
+    #     return False
     
-    def __send_ack(self, result: bool, socket: socket.socket):
-        message = self.protocol.create_ack_msg(result)
-        socket.sendall(message)
+    # def __send_ack(self, result: bool, socket: socket.socket):
+    #     message = self.protocol.create_ack_msg(result)
+    #     socket.sendall(message)
 
-    def __get_confirmation(self, client_sock: socket.socket, action: bytearray) -> int:
-        read_amount = self.protocol.define_buffer_size_confirmation()
-        message = action
-        closed_socket = self.__full_recv(message, read_amount, client_sock)
-        if closed_socket:
-            return -1
-        return self.protocol.decode_confirmation(message)
+    # def __get_confirmation(self, client_sock: socket.socket, action: bytearray) -> int:
+    #     read_amount = self.protocol.define_buffer_size_confirmation()
+    #     message = action
+    #     closed_socket = self.__full_recv(message, read_amount, client_sock)
+    #     if closed_socket:
+    #         return -1
+    #     return self.protocol.decode_confirmation(message)
     
-    def __send_winners(self, clients_map: dict, clients_winners_map: dict):
-        for agency_number, socket in clients_map.items():
-            message = self.protocol.create_winners_msg(clients_winners_map[agency_number])
-            try: 
-                socket.sendall(message)
-            except OSError as e:
-                logging.error(f"action: send_winners | result: fail | error: {e}")
-            finally:
-                # socket.shutdown(socket.SHUT_WR) # allows reading
-                socket.close()
+    # def __send_winners(self, socket: socket.socket, agency_number, has_winners):
+    #     with has_winners:
+    #         logging.info("PRE WAIT")
+    #         has_to_wait = True
+    #         with self.has_winners.get_lock():
+    #             has_to_wait = not self.has_winners.value
+
+    #         if has_to_wait:
+    #             has_winners.wait()
+    #         logging.info("POST WAIT")
+
+    #         message = self.protocol.create_winners_msg(self.winners_map[agency_number])
+    #         try: 
+    #             socket.sendall(message)
+    #         except OSError as e:
+    #             logging.error(f"action: send_winners | result: fail | error: {e}")
+    #         finally:
+    #             # socket.shutdown(socket.SHUT_WR) # allows reading
+    #             socket.close()
             
